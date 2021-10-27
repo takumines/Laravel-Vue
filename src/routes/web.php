88311e91ec48704ade4076c64a2d28d453e7ca54
takumi_kurogi <takumines1@gmail.com>
2021-10-28 00:41:12 +0900

    [update]ルーティング修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+Route::get('/login', [LoginController::class, 'loginForm']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/register', [RegisterController::class, 'registerForm']);
+Route::post('/register', [RegisterController::class, 'register']);
