de850e9bb00c69a0280dc94f9e3ab08a92b26ebc
takumi_kurogi <takumines1@gmail.com>
2021-10-28 00:43:36 +0900

    [update]app.jsコンパイル

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,3 +1,15 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import Vue from 'vue';
+import Login from '../Vue/pages/Login.vue';
+import Register from "../Vue/pages/Register";
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        Login,
+        Register
+    }
+})
